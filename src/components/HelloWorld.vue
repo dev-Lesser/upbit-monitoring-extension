9a826e790e1ebdcfe10094ce9da2b687ad9c38e6
dev-Lesser <dev.jongmoon@gmail.com>
2021-07-11 00:04:05 +0900

    :tada: :construction: initial commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,65 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <v-layout wrap>
+      <v-flex xs12 >
+        <v-card>
+          <v-card-subtitle>
+            {{upbit}}
+          </v-card-subtitle>
+          <v-btn v-on:click="send()">send</v-btn>
+          {{upbit}}
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+import { w3cwebsocket as W3CWebSocket } from "websocket";
+import encoding from "text-encoding";
+  export default {
+    name: 'HelloWorld',
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+    data: () => ({
+      upbit: {
+        "KRW-BTC": null, "KRW-ETH": null,
+      }
+    }),
+    async mounted(){
+      this.connection = new W3CWebSocket("wss://api.upbit.com/websocket/v1")
+      this.connection.binaryType = "arraybuffer";
+      let self = this;
+      this.connection.onmessage = function(event) {
+        // console.log(event)
+        
+        const enc = new encoding.TextDecoder("utf-8");
+        const arr = new Uint8Array(event.data);
+        const data = JSON.parse(enc.decode(arr));
+        console.log(data)
+        self.upbit[data.cd] = data.tp
+      }
+
+      this.connection.onopen = function(event) {
+        console.log(event)
+        console.log("Successfully connected to the echo websocket server...")
+      }
+
+      },
+  
+  methods:{
+    send(){
+      var subscribe_fmt = JSON.stringify([ 
+            {'ticket' :'test'},
+            {
+                "type": "ticker", // ticker -> 현재가, trade -> 체결, orderbook => 호가
+                "codes":["KRW-BTC", "KRW-ETH"], // 수신 시세 종목 정보 (대문자)
+                "isOnlyRealtime": true // 실시간 시세만
+            },
+            {"format":"SIMPLE"} //# simple -> 간소화,
+        ])
+      
+        this.connection.send(subscribe_fmt)
+    }
+  }
+  }
+</script>
