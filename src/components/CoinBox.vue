9a32e268fb9ccdebb5441d75ead0900af6bbfc07
dev-Lesser <dev.jongmoon@gmail.com>
2021-07-12 16:28:36 +0900

    :construction: Add all coins

---
+++ src/components/CoinBox.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-card>
+        <div>
+        {{ coin }}
+        </div>
+        <div>
+        {{ price }}
+        </div>
+    </v-card>
+</template>
+
+<script>
+
+export default ({
+    props:{
+        coin: String,
+        price: Number,
+    }
+})
+</script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,11 +3,8 @@
     <v-layout wrap>
       <v-flex xs12 >
         <v-card>
-          <v-card-subtitle>
-            {{upbit}}
-          </v-card-subtitle>
-          <v-btn v-on:click="send()">send</v-btn>
-          {{upbit}}
+          
+          <coin-box v-for="price,coin in upbit" :coin="coin" :price="price" :key="coin"/>
         </v-card>
       </v-flex>
     </v-layout>
@@ -17,48 +14,60 @@
 <script>
 import { w3cwebsocket as W3CWebSocket } from "websocket";
 import encoding from "text-encoding";
+import CoinBox from "@/components/CoinBox"
+import coinList from "@/assets/coins.json"
   export default {
     name: 'HelloWorld',
-
+    components:{
+      CoinBox
+    },
     data: () => ({
-      upbit: {
-        "KRW-BTC": null, "KRW-ETH": null,
-      }
+      coins: coinList,
+      coinNames : [],
+      coinCode:[],
+      upbit: {}
     }),
+    async created(){
+      this.coins.map(data => {
+        this.coinNames.push(data.coin)
+      })
+      this.coinNames.map(data => {
+        this.upbit[data] = undefined
+      })
+      console.log(this.upbit)
+    },
     async mounted(){
       this.connection = new W3CWebSocket("wss://api.upbit.com/websocket/v1")
       this.connection.binaryType = "arraybuffer";
       let self = this;
-      this.connection.onmessage = function(event) {
-        // console.log(event)
-        
+      this.connection.onmessage = function(event) {        
         const enc = new encoding.TextDecoder("utf-8");
         const arr = new Uint8Array(event.data);
         const data = JSON.parse(enc.decode(arr));
         console.log(data)
         self.upbit[data.cd] = data.tp
       }
-
-      this.connection.onopen = function(event) {
-        console.log(event)
-        console.log("Successfully connected to the echo websocket server...")
-      }
-
-      },
-  
-  methods:{
-    send(){
-      var subscribe_fmt = JSON.stringify([ 
+      
+      this.connection.onopen = function() {
+        var subscribe_fmt = JSON.stringify([ 
             {'ticket' :'test'},
             {
                 "type": "ticker", // ticker -> 현재가, trade -> 체결, orderbook => 호가
-                "codes":["KRW-BTC", "KRW-ETH"], // 수신 시세 종목 정보 (대문자)
+                "codes":self.coinNames, // 수신 시세 종목 정보 (대문자)
                 "isOnlyRealtime": true // 실시간 시세만
             },
             {"format":"SIMPLE"} //# simple -> 간소화,
         ])
       
-        this.connection.send(subscribe_fmt)
+        self.connection.send(subscribe_fmt)
+      }
+     
+
+      },
+  
+  methods:{
+    send(){
+      
     }
   }
   }
